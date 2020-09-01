13fdbd7613455008d32e5bed2ce1cf59bbad0d6c
SilverFox <vzerda@gmail.com>
2020-09-02 02:59:02 +0800

    feat: Modify percentile to make tests like a real-world scenario

--- tests/random-requests.js
+++ tests/random-requests.js
@@ -9,9 +9,9 @@ const statusCodes = [200, 201, 204, 301, 302, 400, 401, 403, 404, 500, 502, 503,
 
 export default function () {
     const delay = Math.random() > 0.5 ? randomIntBetween(1, 20000) : 0;
-    const method = randomItem(methods);
+    const method = Math.random() < 0.4 ? randomItem(methods) : 'GET';
     const prefix = randomItem(prefixes);
     const resource = randomItem(resources);
-    const status = randomItem(statusCodes);
+    const status = Math.random() < 0.3 ? randomItem(statusCodes) : 200;
     http.request(method, `http://localhost:3000${prefix}${resource}?delay=${delay}&status=${status}`);
 }
